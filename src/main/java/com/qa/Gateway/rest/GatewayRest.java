c76f2768a8fa8fdc7e210ddacc256098519be0c4
fortunexfortune <osadolor.fortune@yahoo.it>
2019-02-23 15:31:16 +0000

    added get accounts by cohort id now work

--- src/main/java/com/qa/Gateway/rest/GatewayRest.java
+++ src/main/java/com/qa/Gateway/rest/GatewayRest.java
@@ -1,16 +1,11 @@
 package com.qa.Gateway.rest;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -113,7 +108,18 @@ public class GatewayRest {
     	return response;
     }
     
+    @Value ("${path.genByCohortID}")
+    private String getAccountsByCohortID;
     
+    @GetMapping("${path.getByCohortID}")
+    public Account[] getAccountsByCohortID(@PathVariable Long id) {
+    	return requestGetAccountsByCohortID(id);
+    }
+    
+    private Account[] requestGetAccountsByCohortID(Long id){
+    	Account[] response = restTemplate.getForObject(retrieverURL + getAccountsByCohortID + id, Account[].class);
+    	return response;
+    }
     
 
 }
